75f9f56edcae049700740204ef44c4701cb516c3
CheukHin Mak <jacky.makck@gmail.com>
2020-12-09 23:49:25 +0800

    Initial Commit

--- src/main/java/com/appsdeveloperblog/photoapp/api/gateway/photoappapizuulapigateway/PhotoAppApiZuulApiGatewayApplication.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/gateway/photoappapizuulapigateway/PhotoAppApiZuulApiGatewayApplication.java
@@ -2,10 +2,10 @@ package com.appsdeveloperblog.photoapp.api.gateway.photoappapizuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableEurekaClient
 @EnableZuulProxy
 public class PhotoAppApiZuulApiGatewayApplication {
